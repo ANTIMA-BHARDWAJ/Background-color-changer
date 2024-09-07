69e5cd742daf679464592997946833158ea257ea
ANTIMA-BHARDWAJ <162999970+ANTIMA-BHARDWAJ@users.noreply.github.com>
2024-09-07 01:19:27 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,19 @@
+const button= document.querySelectorAll('.box')
+const body=document.querySelector("body")
+
+button.forEach(function(box){
+    box.addEventListener('click', function(color){
+        if(color.target.id==='box1'){
+            body.style.backgroundColor = 'blue'
+        }
+        if(color.target.id==='box2'){
+            body.style.backgroundColor = 'blueviolet'
+        }
+        if(color.target.id==='box3'){
+            body.style.backgroundColor = 'aqua'
+        }
+        if(color.target.id==='box4'){
+            body.style.backgroundColor = 'brown'
+        }
+    })
+})
